f11af5a816430ed56fa1c8e9dc7227dbec276ac1
YanPanMichael <yanpancsu@icloud.com>
2019-08-13 23:06:11 +0800

    add jquery tabs

--- src/index/App.js
+++ src/index/App.js
@@ -1,17 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import EchartsRadar from './EChart';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import EchartsRadar from "./EChart";
+import "./App.css";
+import Tabs from "./tabs";
 
 class App extends Component {
+  componentDidMount() {
+    const tab = new Tabs({
+      element: "#tab-demo",
+      tabs: "#tab-demo .tabs-nav li",
+      panels: "#tab-demo .tabs-content div",
+      activeIndex: 1
+    });
+    tab.events.on("change", o => {
+      console.log("aaaaaaa", o);
+    });
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Now you are in Index Page.</p>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -21,6 +31,32 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <div id="tab-demo">
+          <div className="tabs-bar" role="tablist">
+            <ul className="tabs-nav">
+              <li role="tab" className="tabs-tab">
+                Tab 1
+              </li>
+              <li role="tab" className="tabs-tab">
+                Tab 2
+              </li>
+              <li role="tab" className="tabs-tab">
+                Tab 3
+              </li>
+            </ul>
+          </div>
+          <div className="tabs-content">
+            <div role="tabpanel" className="tabs-panel">
+              第一个 Tab 里的内容
+            </div>
+            <div role="tabpanel" className="tabs-panel">
+              第二个 Tab 里的内容
+            </div>
+            <div role="tabpanel" className="tabs-panel">
+              第三个 Tab 里的内容
+            </div>
+          </div>
+        </div>
         <EchartsRadar />
       </div>
     );
